3cc67a9bb71898eb05d11692361856a3ab52d827
JimmyWhua <36807484+JimmyWhua@users.noreply.github.com>
2024-12-06 13:18:32 -0600

    Update servomotor.c

--- src/servomotor.c
+++ src/servomotor.c
@@ -19,8 +19,8 @@
 #include "interrupt_support.h"
 
 #define SERVO_PIN           (22)
-#define PULSE_INCREMENT_uS  (INT32_MAX)
-#define SIGNAL_PERIOD_uS    (INT32_MAX)
+#define PULSE_INCREMENT_uS  (500) // 500 or 100 us?
+#define SIGNAL_PERIOD_uS    (20000) // every 20ms mili (20,000μs micro)
 
 static int volatile pulse_width_us;
 
@@ -29,26 +29,68 @@ static void handle_timer_interrupt();
 void initialize_servo() {
     cowpi_set_output_pins(1 << SERVO_PIN);
     center_servo();
-//    register_periodic_timer_ISR(0, PULSE_INCREMENT_uS, handle_timer_interrupt);
+    // register_periodic_timer_ISR(0, PULSE_INCREMENT_uS, handle_timer_interrupt);
+}
+void initialize_servo() {
+    cowpi_set_output_pins(1 << SERVO_PIN);
+    center_servo();
+    register_periodic_timer_ISR(0, PULSE_INCREMENT_uS, handle_timer_interrupt);
 }
 
+
 char *test_servo(char *buffer) {
+    //  Add code to call the center_servo(), rotate_full_clockwise(), and rotate_full_counterclockwise() functions as necessary for Requirements 2-3
+    //  Add code to populate buffer with the strings required by Requirement 4
+    //  Compile and upload your code, and confirm that the correct strings are displayed
+    center_servo();
+    display_string("SERVO: center");
+    rotate_full_clockwise();
+    
+    rotate_full_counterclockwise();
+
+
     ;
     return buffer;
 }
 
-void center_servo() {
-    ;
+void center_servo() {   // 1500μs is 1.5 ms  the central position
+    pulse_width_us = 1500;
 }
 
-void rotate_full_clockwise() {
-    ;
+void rotate_full_clockwise() {  //   500μs pulse directs the servomotor to rotate fully clockwise,
+    pulse_width_us = 500;
 }
 
-void rotate_full_counterclockwise() {
-    ;
+void rotate_full_counterclockwise() {   //  2500μs pulse directs the servomotor to rotate fully counter clockwise
+    pulse_width_us = 2500;
 }
 
+    // ( ) Add code so that when the time until the next rising edge is 0μs:
+        // •Start the pulse by setting output pin 22 to 1.
+// •Update your variable that tracks the time until the next rising edge, to the time
+// until the next pulse should start.
+// •Update your variable that tracks the time until the next falling edge, to the time
+// until this pulse should finish.
+// ( ) Add code so that when the time until the next falling edge is 0μs:
+// •Finish the pulse by setting output pin 22 to 0.
+// ( ) Add code to update the time remaining until the next rising edge and the time until
+// the next falling edge, to reflect the time that has elapsed between timer interrupts.
 static void handle_timer_interrupt() {
-    ;
+//   Add variables to track the time until the next rising edge of the pulse and the next falling edge of the pulse
+    static int32_t rising_edge = 0;
+    static int32_t falling_edge = 0;
+
+// ( ) Add code so that when the time until the next rising edge is 0μs:
+    // •Start the pulse by setting output pin 22 to 1.
+    if (rising_edge <= 0) {     // Start the pulse
+        cowpi_set_output_pins(1<<22);
+        rising_edge = SIGNAL_PERIOD_uS; //  signal period is how often the pulse is sent
+        falling_edge = pulse_width_us;
+    }
+
+    if (falling_edge <= 0) {    // End the pulse
+        cowpi_set_pin(SERVO_PIN, 0);
+    }
+
+    
 }
