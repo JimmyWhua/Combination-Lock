07eeb213ade6578f8b4ee02b7c46a3abbd0ce304
aelhaj2 <152576545+aelhaj2@users.noreply.github.com>
2024-12-08 21:56:34 -0600

    Update rotary-encoder.c

--- src/rotary-encoder.c
+++ src/rotary-encoder.c
@@ -32,46 +32,75 @@ static int volatile counterclockwise_count = 0;
 
 static void handle_quadrature_interrupt();
 
-void initialize_rotary_encoder() {
+void initialize_rotary_encoder() { 
+    //Register interrupt service for pins A & B 
     cowpi_set_pullup_input_pins((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN));
     register_pin_ISR((1 << A_WIPER_PIN) | (1 << B_WIPER_PIN), handle_quadrature_interrupt);
-    state = UNKNOWN;
+    state = UNKNOWN; //initial state set to unknown 
 
 }
 
 uint8_t get_quadrature() {
+    //set up pins stats read for both A & B 
     uint8_t a_signal = get_pio(A_WIPER_PIN);
     uint8_t b_signal = get_pio(B_WIPER_PIN);
+    //Signal shift by 1 bit 
     return (b_signal << 1) | a_signal;
 }
 
 char *count_rotations(char *buffer) {
+    //construct rotation counts to a string
     sprintf(buffer, "CW:%d CCW:%d", clockwise_count, counterclockwise_count);
-
+    //buffer function returns count rotation 
     return buffer;
 }
 
 direction_t get_direction() {
-    direction_t c_direction = direction;
-
+    //store current direction into variable 
+    direction_t current_direction = direction;
+    //reset direction 
     direction = STATIONARY;
-    return STATIONARY;
+    return current_direction;
 }
 
-//This one logic need extra help?!
+//
 static void handle_quadrature_interrupt() {
     static rotation_state_t last_state = UNKNOWN;
     uint8_t quadrature = get_quadrature();
+    //Recognizing clockwise indicators
     switch(quadrature) {
-        case 0b11: state = HIGH_HIGH; break;
-        case 0b10: state = LOW_HIGH; break;
+
         case 0b00: state = LOW_LOW; break;
+        case 0b10: state = LOW_HIGH; break;
         case 0b01: state = HIGH_LOW; break;
-        case state = UNKNOWN; 
+        case 0b01: state = HIGH_LOW; break;
+        case state = UNKNOWN; //state case to be determined 
     }
+    //Recognizing clockwise indicator 
     if (last_state != UNKNOWN && state != UNKNOWN) {
-        if ((last_state == HIGH_HIGH && state == LOW_HIGH)); 
-        ((last_state == LOW_HIGH && state = ))
+
+    if ((last_state == HIGH_HIGH && state == LOW_HIGH) ||
+        (last_state == HIGH_LOW && state = LOW_LOW) ||
+        (last_state == LOW_LOW && state == HIGH_HIGH )||
+        (last_state == HIGH_LOW && state == HIGH_HIGH)) {
+            //set clockwise direction incrementation 
+            clockwise_count++; 
+            //Set direction rotation         
+            direction = clockwise_count; 
+        }
+      
+        //Recognzing counter-clockwise indicator 
+        else if ((last_state == HIGH_HIGH && state == HIGH_LOW) ||
+                (last_state == HIGH_LOW && state == LOW_LOW) ||
+                (last_state == LOW_LOW && state == LOW_HIGH) ||
+                (last_state == LOW_HIGH && state == HIGH_HIGH)) {
+                    //set counterclockwise direction by incrementation
+                    counterclockwise_count++;
+                    //Set direction rotation
+                    direction = counterclockwise; 
+                }
     }
-    ;
+
+    //Last state update 
+    last_state = state; 
 }
